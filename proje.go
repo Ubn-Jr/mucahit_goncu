0bded9f64e8975c0d1413f09395b8049991c9ead
mucahitgoncu <mucahitgoncu@icloud.com>
2022-07-29 12:06:31 +0300

    completed

--- proje.go
+++ proje.go
@@ -4,31 +4,17 @@ import "fmt"
 
 func main() {
 
-	var x int
-	var y int
-	var z int
-	var m int
-	var g int
-	x = -20
-	y = -5
-	z = add(x, y)
-	m = ert(z, x)
-	g = we(m, y)
-	fmt.Println(z, m, g)
-}
+	var x int = 5
+	var y int = 30
+	var z int = x * y
 
-func add(x int, y int) int {
-	var z int
-	z = x * y
-	return z
-}
-func ert(z int, x int) int {
-	var m int
-	m = z - x
-	return m
-}
-func we(m int, y int) int {
-	var g int
-	g = m % y
-	return g
+	fmt.Println("cevap")
+
+	if x > -6 && y >= 30 || x > y && z%x > y+z {
+
+		fmt.Println("dogru")
+	} else {
+
+		fmt.Println("yanlis")
+	}
 }
